2703b29f0c2f60b933566705120020258104bd64
Jose Eduardo Brambila <jose.e.brambila@gmail.com>
2025-12-06 22:39:44 -0300

    Melhorando as interfaces dos metodos

--- Program.cs
+++ Program.cs
@@ -1,82 +1,108 @@
 ﻿using System;
+using System.IO;
 
-/// <summary>
-/// Estes metodos fazem a manipulação de texto em arquivo.
-/// </summary>
-class Program
+namespace TextEditor
 {
-    static void Main(string[] args)
+    public interface IConsole
     {
-        Menu();
+        void Clear();
+        void WriteLine(string value);
+        void Write(string value);
+        string ReadLine();
+        ConsoleKeyInfo ReadKey();
     }
 
-    static void Menu()
+    public class SystemConsole : IConsole
     {
-        Console.Clear();
-        Console.WriteLine("O que você deseja fazer?");
-        Console.WriteLine("1 - Abrir arquivo");
-        Console.WriteLine("2 - Criar novo arquivo");
-        Console.WriteLine("0 - Sair");
+        public void Clear() => Console.Clear();
+        public void WriteLine(string value) => Console.WriteLine(value);
+        public void Write(string value) => Console.Write(value);
+        public string ReadLine() => Console.ReadLine();
+        public ConsoleKeyInfo ReadKey() => Console.ReadKey();
+    }
 
-        short option = short.Parse(Console.ReadLine());
-        switch (option)
+    public class TextEditor
+    {
+        private readonly IConsole _console;
+        public TextEditor(IConsole console) => _console = console;
+
+        public void Menu()
         {
-            case 1:
-                OpenFile(); break;
-            case 2:
-                CreateFile(); break;
-            case 0:
-                Environment.Exit(0); break;
-            default:
-                Menu(); break;
+            _console.Clear();
+            _console.WriteLine("O que você deseja fazer?");
+            _console.WriteLine("1 - Abrir arquivo");
+            _console.WriteLine("2 - Criar novo arquivo");
+            _console.WriteLine("0 - Sair");
+
+            short option = short.Parse(_console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    OpenFile(); break;
+                case 2:
+                    CreateFile(); break;
+                case 0:
+                    Environment.Exit(0); break;
+                default:
+                    Menu(); break;
+            }
+        }
+
+        public void OpenFile()
+        {
+            _console.Clear();
+            _console.WriteLine("Qual o caminho do arquivo?");
+            string path = _console.ReadLine();
+
+            using (var file = new StreamReader(path))
+            {
+                string text = file.ReadToEnd();
+                _console.WriteLine(text);
+            }
+            _console.WriteLine("");
+            _console.ReadLine();
+            Menu();
+        }
+
+        public void CreateFile()
+        {
+            _console.Clear();
+            _console.WriteLine("Digite seu texto abaixo:");
+            _console.WriteLine("-----------------------");
+
+            string text = "";
+
+            do
+            {
+                text += _console.ReadLine() + Environment.NewLine;
+            }
+            while (_console.ReadKey().Key != ConsoleKey.Escape);
+
+            SaveFile(text);
+        }
+
+        public void SaveFile(string text)
+        {
+            _console.Clear();
+            _console.WriteLine("Qual caminho para salvar o arquivo?");
+            var path = _console.ReadLine();
+
+            using (var file = new StreamWriter(path))
+            {
+                file.Write(text);
+            }
+            _console.WriteLine($"Arquivo {path} salvo com sucesso");
+            _console.ReadLine();
+            Menu();
         }
     }
 
-    static void OpenFile()
+    class Program
     {
-        Console.Clear();
-        Console.WriteLine("Qual o caminho do arquivo?");
-        string path = Console.ReadLine();
-
-        using (var file = new StreamReader(path))
+        static void Main(string[] args)
         {
-            string text = file.ReadToEnd();
-            Console.WriteLine(text);
+            var editor = new TextEditor(new SystemConsole());
+            editor.Menu();
         }
-        Console.WriteLine("");
-        Console.ReadLine();
-        Menu();
-    }
-
-    static void CreateFile()
-    {
-        Console.Clear();
-        Console.WriteLine("Digite seu texto abaixo:");
-        Console.WriteLine("-----------------------");
-
-        string text = "";
-
-        do
-        {
-            text += Console.ReadLine() + Environment.NewLine;
-        }
-        while (Console.ReadKey().Key != ConsoleKey.Escape);
-
-        SaveFile(text);
-    }
-
-    static void SaveFile(string text)
-    {
-        Console.Clear();
-        Console.WriteLine("Qual caminho para salvar o arquivo?");
-        var path = Console.ReadLine();
-
-        using (var file = new StreamWriter(path))
-        {
-            file.Write(text);
-        }
-        Console.WriteLine($"Arquivo {path} salvo com sucesso");
-        Console.ReadLine();
-        Menu();
     }
 }
